b3c1d7ab2bb36ae10e5b2dd6aabb600306628a56
JuanAvalo <avalojuanma@gmail.com>
2022-03-31 14:24:44 -0300

    Implemented dotenv for config

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
     // Set your db connection data
     db_mysql: {
-        database: '',
-        host: '',
-        user: '',
-        password: ''
+        database: process.env.DATABASE,
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD
     },
 
     // More info on how to configure this in: https://sendgrid.com/
